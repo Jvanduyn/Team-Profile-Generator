333c233067ed717b9b6121e54a44ca9399c773b5
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-12-18 17:24:29 -0500

    trying to finish last script file that is pushing data to HTML file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,14 +1,27 @@
 function filterTeam(team) {
-    // filter out manager, engineers, interns
-
+    if (role === "manager") {
+        return manager
+    }
+    if (role === "engineer") {
+        return engineer
+    }
+    else () {
+        return engineer
+    }
     // create a function that returns the manager card
+    function returnManager(Manager) {
+
+    }
+
 
     // create a function that returns the intern cards
+    function returnIntern(Intern) {
 
+    }
     // create a function that returns the engineer cards
+    function returnEngineer(Engineer) {
 
-
-
+    }
     // add all cards(html) in to an array
 
     // return the joined array
